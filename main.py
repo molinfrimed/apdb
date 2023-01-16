ee00beb5db69727c17114ee013a9f8c105fe5966
molinfrimed <105166343+molinfrimed@users.noreply.github.com>
2023-01-16 11:54:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, SmartNinja!"
+    return "Hello, this is APDB!"
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run()
